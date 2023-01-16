cd16d1cb7027e430291ea67e2b2dcb43763b8d28
jsoliz064 <jsoliz064@gmail.com>
2023-01-15 22:54:50 -0400

    CU Personal

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -9,47 +9,51 @@
                         <h3 class="d-flex justify-content-center">Personal</h3>
                         @can('personal.create')
                             <a href="{{ route('personal.create') }}" class="btn btn-primary float-right">Registrar Personal</a>
-                        @endcan</div>
+                        @endcan
+                    </div>
                     <div class="card-body">
                         @if (session('success'))
                             <div class="alert alert-success">
                                 {{ session('success') }}
                             </div>
                         @endif
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                    <th>Address</th>
-                                    <th>Type</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($personal as $person)
+                        <div class="table-responsive my-3">
+                            <table class="table table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{ $person->nombre }} {{ $person->apellido }}</td>
-                                        <td>{{ $person->correo }}</td>
-                                        <td>{{ $person->telefono }}</td>
-                                        <td>{{ $person->direccion }}</td>
-                                        <td>{{ $person->TipoPersonal->nombre }}</td>
-                                        <td>
-                                            <a href="{{ route('personal.edit', $person->id) }}"
-                                                class="btn btn-warning">Edit</a>
-                                            <form action="{{ route('personal.destroy', $person->id) }}" method="POST"
-                                                class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger" onclick="return confirm('¿ESTA SEGURO DE  BORRAR?')"
-                                                value="Borrar">Delete</button>
-                                            </form>
-                                        </td>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Address</th>
+                                        <th>Type</th>
+                                        <th>Actions</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($personal as $person)
+                                        <tr>
+                                            <td>{{ $person->nombre }} {{ $person->apellido }}</td>
+                                            <td>{{ $person->correo }}</td>
+                                            <td>{{ $person->telefono }}</td>
+                                            <td>{{ $person->direccion }}</td>
+                                            <td>{{ $person->TipoPersonal->nombre }}</td>
+                                            <td>
+                                                <a href="{{ route('personal.edit', $person->id) }}"
+                                                    class="btn btn-warning">Edit</a>
+                                                <form action="{{ route('personal.destroy', $person->id) }}" method="POST"
+                                                    class="d-inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger"
+                                                        onclick="return confirm('¿ESTA SEGURO DE  BORRAR?')"
+                                                        value="Borrar">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
