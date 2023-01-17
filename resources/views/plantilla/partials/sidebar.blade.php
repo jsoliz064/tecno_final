a92de70cabe7f150869788915aa5410e6002c80c
jsoliz064 <jsoliz064@gmail.com>
2023-01-17 09:49:41 -0400

    ga

--- resources/views/plantilla/partials/sidebar.blade.php
+++ resources/views/plantilla/partials/sidebar.blade.php
@@ -82,12 +82,6 @@
         <li class="nav-heading">Pages</li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" href="users-profile.html">
-                <i class="bi bi-person"></i>
-                <span>Profile</span>
-            </a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link collapsed" href="{{route('asistencias.index')}}">
                 <i class="bi bi-person"></i>
                 <span>Asistencias</span>
