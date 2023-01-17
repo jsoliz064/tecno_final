313c4ef4dbf43a4d5045efba078c2314359c4941
jsoliz064 <jsoliz064@gmail.com>
2023-01-17 10:32:40 -0400

    ga

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -75,12 +75,12 @@ class CertificadoController extends Controller
     }
     public function show(Certificado $certificado)
     {
-        $ruta = "../../../../public".$certificado->qr_path;
+        $ruta = "public".$certificado->qr_path;
         //$ruta=substr($ruta,9,strlen($ruta));
         $imagenBase64 = "";
         //if (Storage::exists($ruta)) {
             //dd("ga");
-            $imagenBase64 = "data:image/png;base64," . base64_encode(file_get_contents($ruta));
+            $imagenBase64 = "data:image/png;base64," . base64_encode(file_get_contents("../".$ruta));
         //}
         $qr = $imagenBase64;
         return view('certificados.show', compact('certificado', 'qr'));
