0afb89391347080a73d8148af8aa67785e070855
jsoliz064 <jsoliz064@gmail.com>
2023-01-17 09:56:20 -0400

    ga

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -75,7 +75,7 @@ class CertificadoController extends Controller
     }
     public function show(Certificado $certificado)
     {
-        $ruta = "../public" . $certificado->qr_path;
+        $ruta = "public" . $certificado->qr_path;
         $imagenBase64 = "";
         if (file_exists("../../".$ruta)) {
             $imagenBase64 = "data:image/png;base64," . base64_encode(file_get_contents($ruta));
