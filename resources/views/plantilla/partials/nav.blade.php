d3f6816cd8ef58fefc56d5cae460554ff1d3a45f
jsoliz064 <jsoliz064@gmail.com>
2023-01-20 12:46:03 -0400

    profile

--- resources/views/plantilla/partials/nav.blade.php
+++ resources/views/plantilla/partials/nav.blade.php
@@ -37,7 +37,7 @@
                         <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}" onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
                             <i class="bi bi-box-arrow-right"></i>
-                            <span>Sign Out</span>
+                            <span>Salir</span>
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
